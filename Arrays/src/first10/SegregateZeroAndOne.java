875d02ef0464418ceb393e1e34f422b7da4361a5
jeetu <jeetu@Jeetu-PC>
2017-05-23 12:08:23 +0530

    Segregate 0s and 1s

---
+++ Arrays/src/first10/SegregateZeroAndOne.java
@@ -0,0 +1,34 @@
+package first10;
+/*You are given an array of 0s and 1s in random order. Segregate 0s on left side and 1s on right side of the array. Traverse array only once.
+
+Input array   =  [0, 1, 0, 1, 0, 0, 1, 1, 1, 0] 
+Output array =  [0, 0, 0, 0, 0, 1, 1, 1, 1, 1] */
+
+public class SegregateZeroAndOne {
+	public static void main(String args[]){
+		int[] arr = new int[]{0, 1, 0, 1, 0, 0, 1, 1, 1, 0};
+		segregateArray(arr);
+		for (int i = 0 ; i < arr.length ; i++){
+			System.out.print(arr[i] + " ");
+		}
+	}
+	
+	private static void segregateArray(int[] arr){
+		int low = 0;
+		int high = arr.length - 1;
+		while (low < high){
+			while (arr[low] == 0 && low < high){
+				low++;
+			}
+			while (arr[high] == 1 && low < high){
+				high--;
+			}
+			if (low < high){
+				arr[low] = 0;
+				arr[high] = 1;
+				low++;
+				high--;
+			}
+		}
+	}
+}
